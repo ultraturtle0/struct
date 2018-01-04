241215a23697f4f9f1d65a0a75c79730d0e5efab
Jordan Kusel <jordankusel@my.unt.edu>
2018-01-03 20:27:34 -0800

    added $watch for validating proximity to last request

--- public/js/controllers/dashboard.client.controllers.js
+++ public/js/controllers/dashboard.client.controllers.js
@@ -58,6 +58,15 @@ angular.module('Database')
                         $scope.vehicles = data;
                     }
                 });
+            JobService
+                .requests
+                .query(query)
+                .$promise
+                .then(function(data) {
+                    if (data.length) {
+                        $scope.requests = data;
+                    }
+                });
         };
 
         var init = function() {
@@ -77,6 +86,20 @@ angular.module('Database')
         $scope.jobs = [];
         $scope.emps = [];
         $scope.select = {};
+        $scope.focus = {};
+
+        $scope.$watch('select.EMP_ID', function (newValue) {
+            
+            var latest = new Date(0);
+            var last;
+            angular.forEach($scope.requests, function(value) {
+                if ((value.EMP_ID == newValue) && (moment(moment().toDate(value.DATE_CREATED)).isAfter(latest))) {
+                    last = value;
+                }
+            });
+            $scope.lastrequest = last;
+        });
+            
 
         $scope.laborSubmit = function () {
             var date = $scope.select.DATE;
