221f52c35d43890891efdb566ae73b0cfac9152d
Jordan Kusel <jordankusel@my.unt.edu>
2018-02-17 13:14:09 -0800

    started pm2 deployment process

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'struct',
+      script    : 'server.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'pi',
+      host : '192.168.1.151',
+      ref  : 'origin/master',
+      repo : 'http://github.com/ultraturtle0/struct.git',
+      path : '/var/www/structdev',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env development',
+      env  : {
+        NODE_ENV: 'development'
+      }
+    }
+  }
+};
