2f2595a5940f55a331730734b01cffd35b17c00c
Jordan Kusel <jordankusel@my.unt.edu>
2018-02-17 18:48:01 -0800

    working on getting acl into api controller

--- config/env/development.js
+++ config/env/development.js
@@ -2,6 +2,7 @@ module.exports = {
     port: 5000,
     domain: 'http://localhost',
     db: 'mongodb://localhost/sss',
+    webmaster: require('./sessionSecret.js'),
     sessionSecret: require('./sessionSecret.js'),
     google_key: require('./googleAPI.js')
 };
